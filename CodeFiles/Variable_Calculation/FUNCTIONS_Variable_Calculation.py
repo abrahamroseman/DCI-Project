6d8bd2b10125bd9bff82c56550f9dffdcd5a0722
Abraham Roseman <abrahamroseman@AbrahamdeMac-mini-111.local>
2025-10-15 23:16:57 +0000

    101525

--- CodeFiles/Variable_Calculation/FUNCTIONS_Variable_Calculation.py
+++ CodeFiles/Variable_Calculation/FUNCTIONS_Variable_Calculation.py
@@ -53,3 +53,51 @@ def CallVariable(ModelData, DataManager, timeString, variableName):
                                                variableName=variableName, dataName=dataName)
     return var_data
 
+
+# In[ ]:
+
+
+# ============================================================
+# CallLagrangianArray_Function
+# ============================================================
+
+import os
+
+def CallLagrangianArray(ModelData, DataManager, timeString, variableName):
+
+    if variableName in ["A_g","A_c","z","x","Z","Y","X"]:
+        dataName = "Lagrangian_Binary_Array"
+        dataType = "LagrangianArrays"
+    elif variableName in ["VMF_g","VMF_c"]:
+        dataName = "Eulerian_VMF"
+        dataType = "LagrangianArrays"
+    elif variableName in ["MSE"]:
+        dataName = "Moist_Static_Energy"
+        dataType = "LagrangianArrays"
+    elif variableName in ["theta_v"]:
+        dataName = "Virtual_Potential_Temperature"
+        dataType = "LagrangianArrays"
+    elif variableName in ["theta_e", "RH_vapor", "RH_ice"]:
+        dataName = "Equivalent_Potential_Temperature"
+        dataType = "LagrangianArrays"
+    elif variableName in ["convergence"]:
+        dataName = "Convergence"
+        dataType = "LagrangianArrays"
+    elif variableName in ["HMC"]:
+        dataName = "MoistureConvergence"
+        dataType = "LagrangianArrays"
+
+    elif variableName in ['e_c','d_c','e_g','d_g']:
+        dataName = "Entrainment"
+        dataType = "LagrangianArrays"
+    elif variableName in ['c_to_g_E','g_to_c_E','c_to_g_D','g_to_c_D']:
+        dataName = "Transfer_Entrainment"
+        dataType = "LagrangianArrays"
+        
+    inputDataDirectory = os.path.normpath(
+        os.path.join(DataManager.outputDirectory, "..", dataType,
+                     f"{DataManager.res}_{DataManager.t_res}_{DataManager.Nz_str}nz", dataName))
+    var_data = DataManager.GetTimestepData(inputDataDirectory, timeString,
+                                           variableName=variableName, dataName=dataName)
+    return var_data
+
