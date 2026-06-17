b0fb1e44b412549344ec9e02153dc15e56c612df
Abraham Roseman <abrahamroseman@dhcp-168-105-251-43.wireless.manoa.hawaii.edu>
2026-06-17 21:58:02 +0000

    061726

--- CodeFiles/3_Project_Algorithms/3_Tracked_Profiles/convert_ipynb_to_py.sh
+++ CodeFiles/3_Project_Algorithms/3_Tracked_Profiles/convert_ipynb_to_py.sh
@@ -7,6 +7,7 @@ NOTEBOOKS=(
     # "Tracked_Profiles_UpdraftArea"
     # "Tracked_Profiles_Plotting_TZContours"
     "Tracked_Ascent_Trajectories"
+    # "EntrainmentTrackback"
 )
 
 # Optional: Load your environment if jupyter isn't in your base path
